60422d688010938b2e919abc9a1a095f77a725ca
niapya <hackhtu@proton.me>
2026-08-02 20:27:40 +0800

    refactor: remove logging functionality and simplify request handling

--- deno.ts
+++ deno.ts
@@ -1,7 +1,7 @@
-import { handleRequest } from "@/lib/log.ts";
 import { router } from "@/router.ts";
 
 Deno.serve(
-  {/* default 8000 */},
-  (request: Request): Promise<Response> => handleRequest(request, router.fetch),
+  (request: Request): Promise<Response> => {
+    return router.fetch(request);
+  },
 );
